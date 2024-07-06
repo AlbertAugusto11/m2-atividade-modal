a1cb2dc7cad8f6308609bf0291147f04aae8e44d
Albert Augusto <augustothelema@gmail.com>
2024-07-05 21:20:34 -0300

    add

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,24 @@
-/* Desenvolva seu código aqui... */
+function openModal(){
+    const modal = document.querySelector(".modalContainer");
+    const buttonOpen = document.querySelector(".form__register-link");
+    buttonOpen.addEventListener("click", () => {
+        modal.showModal();
+
+        closeModal();
+    })
+}
+
+function closeModal(){
+    const modal = document.querySelector(".modalContainer");
+    const buttonClose = document.querySelector(".form__button1");
+    buttonClose.addEventListener("click", () => {
+        modal.close();
+    })
+}
+
+const form = document.querySelector(".form__modal");
+form.addEventListener("submit", (event) => {
+    alert("Cadastro realizado com Sucesso");
+})
+
+openModal()
